db1481cd388db272deb7c58b6d0078df5820c0b3
Nilufar0810 <jumatovanilufarq1999@gmail.com>
2025-05-19 00:07:35 +0500

    animatsiyalar bilan ishlash

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("sommakim!!!");
+// let,var,const
+
+let age = 15
+let number1 = 3.6
+var number2 =2.555
+// console.log(number2=3);
+// console.log(number3);
+
+// 1-misol ikkita sonni yigindisi
+let a = 10;
+let b = 15;
+// let c = a + b;
+// 
